d46663bc90ed4eb040587067aa1701e06cf19f10
Dmitry Simakov <VAGNER2909@yandex.ru>
2017-10-20 19:04:20 +0300

    Changed MainActivity layout

--- app/src/main/java/com/dmitry_simakov/queue/MainActivity.java
+++ app/src/main/java/com/dmitry_simakov/queue/MainActivity.java
@@ -3,7 +3,10 @@ package com.dmitry_simakov.queue;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.dmitry_simakov.queue.fragments.ModelsListFragment;
 
@@ -12,7 +15,12 @@ public class MainActivity extends AppCompatActivity implements ModelsListFragmen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("LOG", "MainActivity: onCreate");
         setContentView(R.layout.activity_main);
+        Log.d("LOG", "setContentView successful");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
+        Log.d("LOG", "setSupportActionBar successful");
     }
     
     @Override
@@ -22,5 +30,22 @@ public class MainActivity extends AppCompatActivity implements ModelsListFragmen
         String modelName = ModelsListFragment.getModelName(id);
         intent.putExtra(ModelActivity.MODEL_NAME, modelName);
         startActivity(intent);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        
+        return super.onOptionsItemSelected(item);
     }
 }
