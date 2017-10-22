e6d95765e7343cd3b0142b05bb0b7415c3681e9d
Dmitry Simakov <VAGNER2909@yandex.ru>
2017-10-22 17:25:45 +0300

    Added GraphFragment

--- app/src/main/java/com/dmitry_simakov/queue/fragments/BarGraphFragment.java
+++ app/src/main/java/com/dmitry_simakov/queue/fragments/BarGraphFragment.java
@@ -1,5 +1,6 @@
 package com.dmitry_simakov.queue.fragments;
 
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -20,13 +21,13 @@ import java.util.List;
 
 public class BarGraphFragment extends Fragment {
     
-    private BarChart barChart;
+    private BarChart chart;
     private float[] XValues;
     private float[] YValues;
     
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_bar_graph, container, false);
     
         Bundle bundle = getArguments();
@@ -36,11 +37,11 @@ public class BarGraphFragment extends Fragment {
             
         }
     
-        barChart = new BarChart(getActivity());
-        barChart.setLayoutParams(new FrameLayout.LayoutParams(
+        chart = new BarChart(getActivity());
+        chart.setLayoutParams(new FrameLayout.LayoutParams(
                 FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
         FrameLayout frameLayout = v.findViewById(R.id.bar_graph_frame);
-        frameLayout.addView(barChart);
+        frameLayout.addView(chart);
     
         List<BarEntry> entries = new ArrayList<BarEntry>();
         for (int i = 0; i < XValues.length; i++) {
@@ -48,47 +49,53 @@ public class BarGraphFragment extends Fragment {
         }
     
         BarDataSet dataSet = new BarDataSet(entries, "График чего-то там");
-        //dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
-        dataSet.setColor(getResources().getColor(R.color.red_700)); // Цвет бара
-        dataSet.setValueTextColor(Color.BLACK);
+    
+        // подготовим цвета
+        Resources res = getResources();
+        int white = res.getColor(R.color.white);
+        int red700 = res.getColor(R.color.red_700);
+        int grey700 = res.getColor(R.color.grey_700);
+        int grey900 = res.getColor(R.color.grey_900);
+        
+        // Настройки столбцов
+        dataSet.setColor(red700);
+        dataSet.setHighLightColor(white);
+    
+        // Настройки значений над столбцами
+        dataSet.setValueTextColor(grey900);
         dataSet.setValueTextSize(16f);
         //dataSet.setDrawValues(false);
-        dataSet.setHighLightColor(Color.WHITE);
+        
+        BarData data = new BarData(dataSet);
+        data.setBarWidth(0.9f); // Ширина бара определяется X величиной
     
-        // Окантовочка
-        //dataSet.setBarBorderColor(Color.BLACK);
-        //dataSet.setBarBorderWidth(1f);
+        // настройки гистограммы
+        chart.setData(data);
+        chart.getDescription().setEnabled(false);
+        chart.setDrawBarShadow(false);
     
-        BarData barData = new BarData(dataSet);
-        barData.setBarWidth(0.9f); // Ширина бара определяется X величиной
+        chart.setDoubleTapToZoomEnabled(false);
+        //chart.setScaleEnabled(false);
+        //chart.setDragEnabled(false);
+        chart.setPinchZoom(true);
+        
+        // настройки осей
+        chart.getAxisLeft().setSpaceBottom(0); // убрать отступ баров от ординаты
+        chart.getAxisRight().setEnabled(false);
     
-        barChart.setData(barData);
-        barChart.getDescription().setEnabled(false);
-        barChart.setDoubleTapToZoomEnabled(false);
-        //barChart.setScaleEnabled(false);
-        //barChart.setDragEnabled(false);
-        barChart.setPinchZoom(true);
-        barChart.setDrawGridBackground(false);
-        barChart.setDrawBarShadow(false);
-    
-        barChart.getAxisLeft().setSpaceBottom(0); // убрать отступ баров от ординаты
-        barChart.getAxisRight().setEnabled(false);
-    
-        XAxis xAxis = barChart.getXAxis();
+        XAxis xAxis = chart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setDrawGridLines(false);
         xAxis.setGranularity(1f); // Минимальные интервалы по X при зуме
         xAxis.setLabelCount(11); // Количество столбцов
     
-        //barChart.invalidate(); // Если нужно перерисовать
-        
         return v;
     }
     
     @Override
     public void onStart() {
         super.onStart();
-        barChart.animateY(1500);
+        chart.animateY(1500);
     }
     
 }
--- app/src/main/java/com/dmitry_simakov/queue/fragments/GraphFragment.java
+++ app/src/main/java/com/dmitry_simakov/queue/fragments/GraphFragment.java
@@ -1,30 +1,112 @@
 package com.dmitry_simakov.queue.fragments;
 
-
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.dmitry_simakov.queue.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class GraphFragment extends Fragment {
     
-    
-    public GraphFragment() {
-        // Required empty public constructor
-    }
-    
+    private LineChart chart;
+    private float[] XValues;
+    private float[] YValues;
     
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_graph, container, false);
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_graph, container, false);
+        
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            XValues = bundle.getFloatArray("XValues");
+            YValues = bundle.getFloatArray("YValues");
+        }
+        
+        chart = new LineChart(getActivity());
+        chart.setLayoutParams(new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
+        FrameLayout frameLayout = v.findViewById(R.id.graph_frame);
+        frameLayout.addView(chart);
+        
+        List<Entry> entries = new ArrayList<Entry>();
+        for (int i = 0; i < XValues.length; i++) {
+            entries.add(new Entry(XValues[i], YValues[i]));
+        }
+        
+        LineDataSet dataSet = new LineDataSet(entries, "График чего-то там");
+        
+        // подготовим цвета
+        Resources res = getResources();
+        int white = res.getColor(R.color.white);
+        int red700 = res.getColor(R.color.red_700);
+        int grey700 = res.getColor(R.color.grey_700);
+        int grey900 = res.getColor(R.color.grey_900);
+        
+        // настройки линии
+        dataSet.setColor(red700);
+        dataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        dataSet.setLineWidth(2f);
+    
+        // настройки кружков
+        dataSet.setCircleColor(red700);
+        //dataSet.setDrawCircleHole(false);
+        dataSet.setCircleColorHole(white);
+        dataSet.setCircleRadius(4f);
+    
+        // настройки креста
+        dataSet.setHighLightColor(grey700);
+        dataSet.setHighlightLineWidth(1.5f);
+    
+        // настройки значений над кружками
+        dataSet.setValueTextColor(grey900);
+        dataSet.setValueTextSize(16f);
+        //dataSet.setDrawValues(false);
+    
+        // настойки заполнения площади под линией
+        //dataSet.setFillColor(Color.RED);
+        //dataSet.setFillAlpha(255);
+        //dataSet.setDrawFilled(true);
+        
+        LineData data = new LineData(dataSet);
+    
+        // настройки графика
+        chart.setData(data);
+        chart.getDescription().setEnabled(false);
+    
+        chart.setPinchZoom(true);
+        chart.setDoubleTapToZoomEnabled(false);
+        
+        // Настройки осей
+        chart.getAxisRight().setEnabled(false);
+        
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        //xAxis.setDrawGridLines(false); // Отключить вертикальные линии
+        xAxis.setGranularity(1f); // Минимальные интервалы по X при зуме
+        xAxis.setLabelCount(11); // Количество отображаемых значений
+        
+        return v;
+    }
+    
+    @Override
+    public void onStart() {
+        super.onStart();
+        chart.animateY(1500);
     }
     
 }
